c558b9841c7fe998b1477ce1e1203b56bd67cb68
eugenleshchov <eugenleshchov@gmail.com>
2017-01-28 13:26:25 +0300

    guessing-game/task: implement gg class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._leftBorder = null;
+        this._rightBorder = null;
+        this._assumption = null;
+    }
 
     setRange(min, max) {
-
+        this._leftBorder = min;
+        this._rightBorder = max;
     }
 
     guess() {
-
+        return this._assumption = this._leftBorder + Math.round((this._rightBorder - this._leftBorder) / 2);
     }
 
     lower() {
-
+        this._rightBorder = this._assumption;
     }
 
     greater() {
-
+        this._leftBorder = this._assumption;
     }
 }
 
